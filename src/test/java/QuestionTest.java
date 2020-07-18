8b431947184d7fb5cb044a1e503dda2c6424d2dd
kevin untrojb <kuntrojb@gmail.com>
2020-07-18 11:24:45 -0300

    T3 | test juego WIP

---
+++ src/test/java/QuestionTest.java
@@ -0,0 +1,25 @@
+
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class QuestionTest {
+
+    @Test
+    public void testCreateATrueFalseResponseQuestionCanCheckTheCorrectAnswer(){
+        String text = "aprobaste algoritmos 3?";
+        Boolean response = false;
+        Boolean correctAnswer = false;
+        Boolean failAnsert = true;
+        Question question = new Question(text,response);
+
+        Assert.assertEquals(question.Answer(),correctAnswer);
+        Assert.assertNotEquals(question.Answer(),failAnsert);
+    }
+
+    @Test
+    public void testCreateATrueFalseResponseQuestionCanCheckAListOfAnswers(){
+
+    }
+}
