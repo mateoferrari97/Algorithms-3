31017da42679a20b3b50029bb931a52081dd73bd
BautistaXifro <bauxifro@gmail.com>
2020-07-21 13:55:50 -0300

    T2 | cambio el texto de la pregunta

--- src/test/java/modelo/QuestionTest.java
+++ src/test/java/modelo/QuestionTest.java
@@ -10,10 +10,10 @@ public class QuestionTest {
 
     @Test
     public void testCreateATrueFalseResponseQuestionCanCheckTheCorrectAnswer(){
-        String text = "aprobaste algoritmos 3?";
-        Boolean correctAnswer = false;
-        Boolean failAnswer = true;
-        Question question = new Question(text,false);
+        String text = "vamos a aprobar algoritmos 3?";
+        Boolean correctAnswer = true;
+        Boolean failAnswer = false;
+        Question question = new Question(text,true);
 
         Assert.assertEquals(question.getAnswer(),correctAnswer);
         Assert.assertNotEquals(question.getAnswer(),failAnswer);
@@ -22,13 +22,13 @@ public class QuestionTest {
     @Test
     public void testCreateATrueFalseResponseQuestionCanCheckAListOfAnswers(){
         //Given
-        String text = "aprobaste algoritmos 3?";
-        Question question = new Question(text,false);
+        String text = "vamos a aprobar algoritmos 3?";
+        Question question = new Question(text,true);
         Player playerOne = new Player();
         Player playerTwo = new Player();
         Integer expectedPlayerOnePoints = 1;
         Integer expectedPlayerTwoPoints = 0;
-        Boolean[] answers = {false,true};
+        Boolean[] answers = {true,false};
         Player[] players = {playerOne,playerTwo};
 
         //When
