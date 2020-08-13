0bca6a39a00dee2145738227cc207059b76fcdcb
BautistaXifro <bauxifro@gmail.com>
2020-08-13 18:21:15 -0300

    T14 | borro las clases round y turn de interfaz grafica

--- src/main/java/interfazGrafica/Eventos/BotonEnviarEventHandler.java
+++ src/main/java/interfazGrafica/Eventos/BotonEnviarEventHandler.java
@@ -1,18 +1,13 @@
 package interfazGrafica.Eventos;
 
 import interfazGrafica.Play;
-import interfazGrafica.Round;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import modelo.Game;
 import modelo.Player;
-
-import java.util.List;
 
 public class BotonEnviarEventHandler implements EventHandler<ActionEvent> {
 
